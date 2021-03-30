4dfc596aef2613aa9f6c7f4637d2ff0c3caaf720
Caio Luis <caio.oliveira@truckpad.com.br>
2021-03-29 21:04:39 -0300

    base structure added for clean architecture with modularization

---
+++ buildSrc/src/main/java/dependencies.kt
@@ -0,0 +1,51 @@
+/**
+ * Created by Caio Luis (caio-luis) on 29/03/21
+ */
+
+object CoreDependencies {
+    const val ktxCore = "androidx.core:core-ktx:${KotlinCoreVersions.ktxCoreVersion}"
+    const val kotlinStdLib =
+        "org.jetbrains.kotlin:kotlin-stdlib:${KotlinCoreVersions.kotlinVersion}"
+    const val appCompat = "androidx.appcompat:appcompat:${KotlinCoreVersions.appCompatVersion}"
+    const val gradleAndroid = "com.android.tools.build:gradle:${BuildVersions.androidGradle}"
+    const val gradleKotlin =
+        "org.jetbrains.kotlin:kotlin-gradle-plugin:${KotlinCoreVersions.kotlinVersion}"
+}
+
+object TestDependencies {
+    const val jUnit = "androidx.test.ext:junit:${TestVersions.androidxJunit}"
+    const val assertJ = "org.assertj:assertj-core:${TestVersions.assertJ}"
+    const val androidTestCore = "androidx.test:core:${TestVersions.androidTestArchCore}"
+    const val androidTestRunner = "androidx.test:runner:${TestVersions.androidTextRunner}"
+    const val androidTestRules = "androidx.test:rules:${TestVersions.androidTextRules}"
+    const val espresso = "androidx.test.espresso:espresso-core:${TestVersions.espresso}"
+    const val coreTesting = "androidx.arch.core:core-testing:${TestVersions.coreTesting}"
+}
+
+object Dependencies {
+    const val constraintLayout =
+        "androidx.constraintlayout:constraintlayout:${DesignVersions.constraintLayoutVersion}"
+
+    const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+    const val retrofitGsonConverter = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+
+    const val gson = "com.google.code.gson:gson:${Versions.gson}"
+
+    const val koin = "org.koin:koin-core:${Versions.koin}"
+    const val koinAndroid = "org.koin:koin-android:${Versions.koin}"
+    const val koinViewModel = "org.koin:koin-androidx-viewmodel:${Versions.koin}"
+    const val materialDesign = "com.google.android.material:material:${Versions.koin}"
+
+    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+
+    const val lifeCycle = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
+    const val lifeCycleKapt = "androidx.lifecycle:lifecycle-compiler:${Versions.lifecycle}"
+    const val lifeCycleKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle}"
+    const val lifeCycleViewModel =
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+
+    const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
+    const val cardView = "androidx.cardview:cardview:${Versions.cardView}"
+    const val swipeRefreshLayout =
+        "androidx.swiperefreshlayout:swiperefreshlayout:${Versions.swipeRefreshLayout}"
+}
---
+++ buildSrc/src/main/java/versions.kt
@@ -0,0 +1,38 @@
+/**
+ * Created by Caio Luis (caio-luis) on 29/03/21
+ */
+
+internal object KotlinCoreVersions {
+    const val ktxCoreVersion = "1.3.2"
+    const val kotlinVersion = "1.4.31"
+    const val appCompatVersion = "1.2.0"
+}
+
+internal object BuildVersions {
+    const val androidGradle = "4.1.3"
+}
+
+internal object DesignVersions {
+    const val constraintLayoutVersion = "2.0.4"
+}
+
+internal object Versions {
+    const val retrofit = "2.9.0"
+    const val gson = "2.8.6"
+    const val koin = "3.0.0-alpha-1"
+    const val coroutines = "1.4.3"
+    const val lifecycle = "2.3.1"
+    const val recyclerView = "1.1.0"
+    const val cardView = "1.0.0"
+    const val swipeRefreshLayout = "1.0.0"
+}
+
+internal object TestVersions {
+    const val assertJ = "3.11.1"
+    const val androidxJunit = "4.13.2"
+    const val espresso = "3.3.0"
+    const val androidTestArchCore = "1.0.0"
+    const val androidTextRules = "1.1.0"
+    const val androidTextRunner = "1.1.0"
+    const val coreTesting = "2.1.0"
+}
